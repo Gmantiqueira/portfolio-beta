0612776c98cd8e22a7262ad6e4b2dc1f8f5c8c80
Gmantiqueira <gmantiqueira@outlook.com>
2019-03-11 23:12:27 -0300

    Desenvolvendo estrutura do site

--- js/main.js
+++ js/main.js
@@ -1,7 +1,50 @@
 $("#menu-toggle").click(function() {
   $(".menu").toggleClass("toggle");
+  $(this).toggleClass("close");
+  $("span").toggleClass("close");
 });
 
-$("#close-btn").click(function() {
-  $(".menu").toggleClass("toggle");
+$(".menu a").click(function() {
+  if ($(window).width() < 992) {
+    $(".menu").toggleClass("toggle");
+    $("#menu-toggle").toggleClass("close");
+    $("span").toggleClass("close");
+  }
+});
+
+$(document).scroll(function() {
+  $(".logo-fill-white").each(function() {
+    var sectionHeight = $(this).height();
+    var sectionOffset = $(this).offset().top - 200;
+    var sectionOffsetBottom = sectionOffset + sectionHeight;
+
+    if (scroll_pos > sectionOffset && scroll_pos < sectionOffsetBottom) {
+      $(".logo").addClass("white");
+      $(".logo").removeClass("dark");
+      $(".menu-toggle span").addClass("white");
+      $(".menu-toggle span").removeClass("dark");
+    }
+  });
+
+  $(".logo-fill-dark").each(function() {
+    var sectionHeight = $(this).height();
+    var sectionOffset = $(this).offset().top - 200;
+    var sectionOffsetBottom = sectionOffset + sectionHeight;
+
+    if (scroll_pos > sectionOffset && scroll_pos < sectionOffsetBottom) {
+      $(".logo-letter").addClass("blue");
+      $(".logo-letter").removeClass("white");
+    }
+  });
+});
+
+var $doc = $("html, body");
+$(".menu a").click(function() {
+  $doc.animate(
+    {
+      scrollTop: $($.attr(this, "href")).offset().top - 80
+    },
+    400
+  );
+  return false;
 });
